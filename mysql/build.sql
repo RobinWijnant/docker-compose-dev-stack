6dd88d6d62872afca5470443201b5e617244e672
Robin Wijnant <im2opc@gmail.com>
2018-06-29 01:00:01 +0200

    caching_sha2 user

--- mysql/build.sql
+++ mysql/build.sql
@@ -7,10 +7,10 @@
 ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root';
 ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'root';
 
--- Create a new root user with sha256_password auth method
+-- Create a new root user with caching_sha2_password auth method
 -- Use this user if you want to use the new and fancy method
 -- Note: This method requires an SSL/TLS setup
-CREATE USER 'root_sha2'@'%' IDENTIFIED WITH sha256_password BY 'root';
+CREATE USER 'root_sha2'@'%' IDENTIFIED WITH caching_sha2_password BY 'root';
 GRANT ALL PRIVILEGES ON * . * TO 'root_sha2'@'%' WITH GRANT OPTION;
 
 FLUSH PRIVILEGES;
